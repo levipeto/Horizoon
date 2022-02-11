23bd5ed941406c6b2d0f436e2b7ebbb1342be1b1
alexsol22 <alessiosolari22@gmail.com>
2022-02-11 11:41:29 +0100

    commits

--- public/js/admin/panel.js
+++ public/js/admin/panel.js
@@ -1,19 +1,19 @@
-"use strict";
+// "use strict";
 
-let menu_panel_btn = document.querySelector(".menu");
-let menu_items = document.querySelector(".menu-items");
-let overlay = document.querySelector(".overlay");
+// let menu_panel_btn = document.querySelector(".menu");
+// let menu_items = document.querySelector(".menu-items");
+// let overlay = document.querySelector(".overlay");
 
-menu_panel_btn.addEventListener("click", function (e) {
-    menu_items.classList.remove("-translate-x-full");
-    overlay.classList.remove("hidden");
-    document.body.style.overflow = "hidden";
-});
+// menu_panel_btn.addEventListener("click", function (e) {
+//     menu_items.classList.remove("-translate-x-full");
+//     overlay.classList.remove("hidden");
+//     document.body.style.overflow = "hidden";
+// });
 
-window.addEventListener('click',function(e){
-    if(e.target.classList.contains('overlay')){
-      overlay.classList.add("hidden");
-      menu_items.classList.add("-translate-x-full");
-      document.body.style.overflow = "auto";
-    }
-});
+// window.addEventListener('click',function(e){
+//     if(e.target.classList.contains('overlay')){
+//       overlay.classList.add("hidden");
+//       menu_items.classList.add("-translate-x-full");
+//       document.body.style.overflow = "auto";
+//     }
+// });
